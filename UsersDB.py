77fee9c887faa97e52cb1eab227e8da602afc923
rudrajiii <rudrasaha305@gmail.com>
2024-04-25 19:08:33 +0530

    m

--- UsersDB.py
+++ UsersDB.py
@@ -5,4 +5,3 @@ cursor = connection.cursor()
 connection.execute("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT , email TEXT, gender TEXT, contact TEXT, dob date, Enrollment_no TEXT , profile_pic BLOB)")
 print("Table created successfully")
 connection.close()   
-#comment added
