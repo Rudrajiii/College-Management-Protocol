17e06d41b074d34e29eb398e08ca3528266b5001
rudrajiii <rudrasaha305@gmail.com>
2024-05-01 03:19:07 +0530

    new fields are added

--- crud.py
+++ crud.py
@@ -5,11 +5,12 @@ from PIL import Image
 from datetime import  datetime
 from resizeimage import resizeimage
 import glob
+import random
 import sqlite3
 import os
 import csv
 app = Flask(__name__)
-csv_file_path = 'student_data.csv'
+csv_file_path = 'data/modified_student_data.csv'
 app.config['UPLOAD_DIR'] = 'static/Uploads'
 root_dir = 'static/Uploads'
 def get_post(id):
@@ -35,6 +36,48 @@ def read_csv(your_csv_file):
         csv_file = csv.DictReader(csv_reader)
         data = [row for row in csv_file]
     return data
+def modified_csv_data():
+    # Given departments array
+    departments_array = ['CSE', 'CSE(ai)', 'CSE(ai & Ml)', 'ECE', 'EE', 'ME', 'IOT', 'CSBS', 'IT']
+
+    # Read the CSV file and store the data
+    data = []
+    with open('data/student_data.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append(row)
+    print(data)
+# Count the number of rows and calculate the desired number of CSE entries
+    total_rows = len(data)
+    cse_min_count = total_rows * 0.4
+    cse_max_count = total_rows * 0.6
+
+    # Initialize counters
+    cse_count = 0
+    new_data = []
+
+    # Modify department values according to the given array
+    for row in data:
+        if row['department'] == 'CSE':
+            if cse_count < cse_min_count:
+                row['department'] = 'CSE'
+                cse_count += 1
+            elif cse_count >= cse_min_count and cse_count < cse_max_count:
+                row['department'] = random.choice(['CSE', 'CSE(ai)', 'CSE(ai & Ml)'])
+                cse_count += 1
+            else:
+                row['department'] = random.choice(departments_array)
+        else:
+            row['department'] = random.choice(departments_array)
+        new_data.append(row)
+
+# Write the modified data to a new CSV file
+    with open('data/modified_student_data.csv', 'w', newline='') as csvfile:
+        fieldnames = ['Full_name', 'gender', 'department']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in new_data:
+            writer.writerow(row)
 
 @app.route("/access_data")
 def access_data():
@@ -155,4 +198,5 @@ def delete_user(id):
     
 
 if __name__ == "__main__":
-    app.run(debug = True)  
+    app.run(debug = True)
+    modified_csv_data()  
