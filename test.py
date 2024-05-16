a8220d2d973b5af49771191e25bc3c0c9a7a8cdc
rudrajiii <rudrasaha305@gmail.com>
2024-05-17 00:47:17 +0530

    push

---
+++ test.py
@@ -0,0 +1,16 @@
+print('helo')
+
+import pymongo
+
+def admin_login_db(enrollment_no,username,password):
+    client = pymongo.MongoClient("mongodb+srv://sambhranta1123:SbGgIK3dZBn9uc2r@cluster0.jjcc5or.mongodb.net/")
+    # Acessing project Database
+    db = client['project']
+    # Acessing creators/admins Collection
+    collection = db.creators
+    admins = collection.find_one({"username": username , "enrollment_no": enrollment_no , "password": password})
+    if admins == None:
+        return 0
+    else:
+        return 1
+admin_login_db('xxxx' , "Rudra" , '123')
