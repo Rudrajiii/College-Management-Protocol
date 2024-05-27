26a3ecdb911345efa4fbfc05fe64abaeebe1e954
rudrajiii <rudrasaha305@gmail.com>
2024-05-27 18:02:47 +0530

    teachers pic

---
+++ sysProj/teacher_info.py
@@ -0,0 +1,5 @@
+teacher_image_source = [
+    'https://github.com/Rudrajiii/Recipe-App/blob/main/public/images/uploads/profilePic1716407178121.jpg?raw=true',
+    'https://github.com/Rudrajiii/Recipe-App/blob/main/public/images/uploads/profilePic1716411519160.jpg?raw=true',
+    'https://github.com/Rudrajiii/Recipe-App/blob/main/public/images/uploads/profilePic1716274121725.png?raw=true'
+]
