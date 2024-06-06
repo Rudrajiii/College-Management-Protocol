c0fca4f30078022d27d9e71fdcfe485acbe7062f
rudrajiii <rudrasaha305@gmail.com>
2024-06-06 18:13:28 +0530

    html and python garbage file removed

--- sysProj/crud.py
+++ sysProj/crud.py
@@ -486,7 +486,7 @@ def delete_user(user_id):
 
 
 
-#inserted code from satyadeep
+#*inserted code from satyadeep
 # Edited start by satyadeep at 3/6/24
 # Add student details route
 
@@ -616,21 +616,7 @@ def logout():
     return redirect(url_for('index'))
 
 
-#!No to need to present in this file
-#todo Here is a problem have to fix it later!!!
-@app.route('/login', methods=['POST'])
-def login():
-    auth = request.authorization
-    if auth and auth.username == 'admin' and auth.password == 'password':
-        return jsonify({'message': 'Login successful'}), 200
-    else:
-        return jsonify({'error': 'Invalid credentials'}), 401
-
-#!No to need to present in this file
-@app.route("/student_dashBoard")
-def student():
-    student_data = read_csv(csv_file_path)
-    return render_template("student_dash_board.html" , data=student_data)
+#** Important function for chart data generation
 
 def read_csv(your_csv_file):
     with open(your_csv_file, "r") as csv_reader:
@@ -680,108 +666,10 @@ def modified_csv_data():
         for row in new_data:
             writer.writerow(row)
 
-#!No to need to present in this file
 @app.route("/access_data")
 def access_data():
     student_data = read_csv(csv_file_path)
     return jsonify(student_data)
-
-@app.route("/add")
-def add():   
-    return render_template("add.html")
-
-#!No to need to present in this file
-@app.route("/logic")
-def log():
-    return Enrollment_logs()
-
-#!No to need to present in this file
-@app.route("/savedetails",methods = ["POST","GET"])
-def saveDetails():
-    msg = "msg"
-    if request.method == "POST":
-        try:
-            name = request.form["name"]
-            email = request.form["email"]
-            gender = request.form["gender"]
-            contact = request.form["contact"]
-            dob = request.form["dob"]
-            Enrollment_no = request.form["Enrollment_no"]
-            file = request.files["profile_pic"]
-            file.save(os.path.join(app.config['UPLOAD_DIR'],file.filename))
-            with sqlite3.connect("users.db") as con:
-                cur = con.cursor()   
-                cur.execute("INSERT into users(name, email, gender, contact, dob, Enrollment_no, profile_pic) values (?,?,?,?,?,?,?)",(name,email,gender,contact,dob,Enrollment_no,file.filename))
-                con.commit()
-                msg = "User successfully Added"   
-        except:
-            con.rollback()
-            msg = "We can not add User to the list"
-        finally:
-            con.close()
-            return render_template("success.html",msg = msg)
-
-@app.route("/view")
-def view():    
-    con = sqlite3.connect("users.db")
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    cur.execute("select * from users")   
-    rows = cur.fetchall()
-    return render_template("view.html",rows = rows,)
-
-@app.route("/<int:id>/view_user", methods=("GET", "POST"))
-def view_user(id):
-    row = get_post(id)
-    with sqlite3.connect("users.db") as con:
-        cur = con.cursor()
-        filename_or = row["profile_pic"]
-        packages =  con.execute('Select date(dob) FROM users WHERE id = ?', (id,)).fetchone()
-        for dob in packages:
-            dob = datetime.strptime(dob, '%Y-%m-%d')
-            age = (datetime.today() - dob).days/365
-            age = round(age, 1)
-    return render_template("view_user.html",row = row, now_date = age,)
-
-@app.route("/<int:id>/resize_user", methods=("GET", "POST"))
-def resize_user(id):
-    row = get_post(id)
-    with sqlite3.connect("users.db") as con:
-        cur = con.cursor()
-        filename_or = row["profile_pic"]
-        with open('static/Uploads/' + filename_or, 'r+b') as f:
-            with Image.open(f) as image:
-                cover = resizeimage.resize_cover(image, [200, 200])
-                cover.save('static/Uploads/' + filename_or, image.format)      
-    return render_template("index.html")
-
-@app.route("/<int:id>/edit_user", methods=("GET", "POST"))
-def edit_user(id):
-    user = get_post(id)
-
-    if request.method == 'POST':
-        name = request.form["name"]
-        email = request.form["email"]
-        gender = request.form["gender"]
-        contact = request.form["contact"]
-        dob = request.form["dob"]
-        Enrollment_no = request.form["Enrollment_no"]
-        with sqlite3.connect("users.db") as con:
-            cur = con.cursor()
-            query = '''
-                UPDATE users
-                SET name = ?, email = ?, gender = ?, contact = ?, dob = ? , Enrollment_no = ? where id = ?
-            '''
-            cur.execute(query,(name,email,gender,contact,dob,Enrollment_no,id))
-            con.commit()
-            msg = "Updated Successfully!!"
-            return render_template("success.html" , msg = msg)
-    return render_template('edit_user.html', user = user)
-
-#!No to need to present in this file
-@app.route('/secret')
-def secret():
-    return get_data()
 
 
 #* Error handling
@@ -797,4 +685,5 @@ def internal_server_error(e):
 if __name__ == "__main__":
     app.run(debug = True)
     modified_csv_data()
-    test('Rudra')  
+    test('Rudra')
+
